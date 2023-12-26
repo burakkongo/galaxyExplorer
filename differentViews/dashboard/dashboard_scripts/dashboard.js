04b5c6b4a0218addd17ee19979ef4d8fba9a7ea1
Burak Kongo <burak.kongo@stud.fh-campuswien.ac.at>
2023-12-26 19:55:05 +0100

    Adds the dashboard functionality without further designing.

---
+++ differentViews/dashboard/dashboard_scripts/dashboard.js
@@ -0,0 +1,38 @@
+function navigateTo(subject) {
+
+    // Basispfad zu den Flashcard-Views
+    const baseFlashcardsViewPath = '../flashcardsViews/';
+
+    // Name der HTML-Seite, die die Flashcards für das gewählte Fach enthält
+    let subjectPage = '';
+
+    switch (subject) {
+        case 'biology':
+            subjectPage = 'biology.html';
+            break;
+        case 'chemistry':
+            subjectPage = 'chemistry.html';
+            break;
+        case 'geography':
+            subjectPage = 'geography.html';
+            break;
+        case 'history':
+            subjectPage = 'history.html';
+            break;
+        case 'mathematics':
+            subjectPage = 'mathematics.html';
+            break;
+        case 'programming':
+            subjectPage = 'programming.html';
+            break;
+        case 'map':
+            window.location.href = '../adventureMap/adventure_map.html';
+            return;
+        default:
+            console.error('Subject not found');
+            return;
+    }
+
+    // Ändert die aktuelle URL zur Ziel-URL, was die Navigation zur neuen Seite auslöst
+    window.location.href = baseFlashcardsViewPath + subjectPage;
+}
