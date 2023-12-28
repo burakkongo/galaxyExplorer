f590d5f0d349278cfeeae9ab73b46569cec70c14
fn747 <92045446+fn747@users.noreply.github.com>
2023-12-28 21:39:51 +0100

    Fixed server path issues. Fixed user registration

--- registration/registration_scripts/registration.js
+++ registration/registration_scripts/registration.js
@@ -1,6 +1,1 @@
-    document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('myButton').addEventListener('click', function() {
-        // Navigate to the register.html page
-        window.location.href = '/register';
-    });
-});
+
