fad3a8d6ab7cebaae4210b9b133723db0a9791f1
Burak Kongo <burak.kongo@stud.fh-campuswien.ac.at>
2023-12-29 18:39:03 +0100

    Minor changes and improvements.

--- landingPage/landingPage_scripts/landingPage.js
+++ landingPage/landingPage_scripts/landingPage.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('myButton').addEventListener('click', function() {
-        // Navigate to the register.html page
+        // Navigate to the registration.html page
         window.location.href = '/register';
     });
 });
